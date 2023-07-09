b8d31405855db03b9289fa38197aeb87638210fb
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-07-09 13:50:05 +0100

    Update CPU_Temperature_Checker.py

--- src/CPU_Temperature_Checker.py
+++ src/CPU_Temperature_Checker.py
@@ -1,1 +1,14 @@
+import psutil
 
+def get_cpu_temperature():
+    temperature = psutil.sensors_temperatures().get('coretemp', [])
+    if temperature:
+        return temperature[0].current
+    else:
+        return None
+# Retrieve CPU temperature
+cpu_temperature = get_cpu_temperature()
+if cpu_temperature is not None:
+    print(f"CPU temperature: {cpu_temperature}°C")
+else:
+    print("Failed to retrieve CPU temperature.")
